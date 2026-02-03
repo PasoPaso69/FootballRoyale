2b9cb16d0a8160c0b26a1b5704c2b782c1b7ff53
TheMethod03 <matmarino03@gmail.com>
2026-02-03 18:42:01 +0100

    divisione del codice, costruttori e relazioni

---
+++ src/main/java/com/mycompany/footballroyale/service/FootballRoyaleBusinessFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.footballroyale.service;
+
+/**
+ *
+ * @author Lenovo
+ */
+//dovrebbe essere classe Singleton/Factory che serve per creare i due gestori
+public class FootballRoyaleBusinessFactory {
+    
+}
